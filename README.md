3bb9c46da3d5d7ebbb4f6c08b80c0d2567d8fc67
TakWolf <takwolf@foxmail.com>
2024-06-25 17:16:29 +0800

    基本项目结构

---
+++ README.md
@@ -0,0 +1,22 @@
+# Pixel Font Knife
+
+[![Python](https://img.shields.io/badge/python-3.10-brightgreen)](https://www.python.org)
+[![PyPI](https://img.shields.io/pypi/v/pixel-font-knife)](https://pypi.org/project/pixel-font-knife/)
+
+一组像素字体实用工具。
+
+该库被设计用于和 [Pixel Font Builder](https://github.com/TakWolf/pixel-font-builder) 配合使用，也包含一些符合 [Ark Pixel Font](https://github.com/TakWolf/ark-pixel-font) 构建约定的通用工具。
+
+## 安装
+
+```shell
+pip install pixel-font-knife
+```
+
+## 程序依赖
+
+- [PyPNG](https://gitlab.com/drj11/pypng)
+
+## 许可证
+
+基于 [MIT 许可证](LICENSE)。
---
+++ tests/test_mono_bitmap_util.py
@@ -0,0 +1,35 @@
+import hashlib
+from pathlib import Path
+
+from pixel_font_knife import mono_bitmap_util
+
+project_root_dir = Path(__file__).parent.joinpath('..').resolve()
+
+
+def _file_sha256(file_path: Path) -> str:
+    return hashlib.sha256(file_path.read_bytes()).hexdigest()
+
+
+def test_load_save(tmp_path: Path):
+    load_file_path = project_root_dir.joinpath('assets', '4E2D.png')
+    save_file_path = tmp_path.joinpath('4E2D.png')
+
+    bitmap, width, height = mono_bitmap_util.load_png(load_file_path)
+    assert bitmap == [
+        [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0],
+        [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0],
+        [1, 0, 0, 0, 0, 1, 0, 0, 0, 0, 1, 0],
+        [1, 0, 0, 0, 0, 1, 0, 0, 0, 0, 1, 0],
+        [1, 0, 0, 0, 0, 1, 0, 0, 0, 0, 1, 0],
+        [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0],
+        [0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0]
+    ]
+    assert width == height == 12
+
+    mono_bitmap_util.save_png(bitmap, save_file_path)
+    assert _file_sha256(load_file_path) == _file_sha256(save_file_path)
