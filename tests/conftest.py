a61af744a4c3fda6207783a1660bc19bbda41eb2
TakWolf <takwolf@foxmail.com>
2024-07-04 11:44:45 +0800

    测试 fixture 添加 glyphs_dir

--- tests/conftest.py
+++ tests/conftest.py
@@ -11,3 +11,8 @@ def project_root_dir() -> Path:
 @pytest.fixture(scope='session')
 def assets_dir(project_root_dir: Path) -> Path:
     return project_root_dir.joinpath('assets')
+
+
+@pytest.fixture(scope='session')
+def glyphs_dir(assets_dir: Path) -> Path:
+    return assets_dir.joinpath('glyphs')
