8ef27f9802396a7a2d8a459874a6367786527cec
TakWolf <takwolf@foxmail.com>
2024-07-04 14:34:51 +0800

    实现 MonoBitmap.cost()

--- src/pixel_font_knife/mono_bitmap.py
+++ src/pixel_font_knife/mono_bitmap.py
@@ -2,6 +2,17 @@ from collections import UserList
 
 
 class MonoBitmap(UserList[list[int]]):
+    @staticmethod
+    def cost(source: list[list[int]]) -> 'MonoBitmap':
+        bitmap = MonoBitmap(0, 0)
+        bitmap.width = len(source[0])
+        bitmap.height = len(source)
+        for source_row in source:
+            if bitmap.width != len(source_row):
+                raise ValueError('Rows widths unequal')
+            bitmap.append([0 if alpha == 0 else 1 for alpha in source_row])
+        return bitmap
+
     width: int
     height: int
 
--- tests/test_mono_bitmap.py
+++ tests/test_mono_bitmap.py
@@ -1,3 +1,5 @@
+import pytest
+
 from pixel_font_knife.mono_bitmap import MonoBitmap
 
 
@@ -20,3 +22,22 @@ def test_create():
         [1, 1],
         [1, 1],
     ]
+
+
+def test_cost():
+    bitmap = MonoBitmap.cost([
+        [0, 1, 2, 3],
+        [1, 0, 0, 1],
+    ])
+    assert bitmap.width == 4
+    assert bitmap.height == 2
+    assert bitmap == [
+        [0, 1, 1, 1],
+        [1, 0, 0, 1],
+    ]
+
+    with pytest.raises(ValueError):
+        MonoBitmap.cost([
+            [0, 1, 2],
+            [],
+        ])
