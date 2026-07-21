5196c65c22373688bf4933d4e6dccdd6d81bad11
TakWolf <takwolf@foxmail.com>
2026-07-21 10:45:21 +0800

    规范扩展名 .yml -> .yaml

--- tests/test_glyph_mapping_util.py
+++ tests/test_glyph_mapping_util.py
@@ -4,8 +4,8 @@ from pixel_font_knife import glyph_mapping_util, glyph_file_util
 
 
 def test_load(assets_dir: Path, glyphs_dir: Path, tmp_path: Path):
-    load_path = assets_dir.joinpath('mapping-example.yml')
-    save_path = tmp_path.joinpath('mapping-example.yml')
+    load_path = assets_dir.joinpath('mapping-example.yaml')
+    save_path = tmp_path.joinpath('mapping-example.yaml')
 
     mapping = glyph_mapping_util.load_mapping(load_path)
     glyph_mapping_util.save_mapping(mapping, save_path)
