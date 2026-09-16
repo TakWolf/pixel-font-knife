70ddbfe74b43c257a95a40e6f447aa87a38c53e5
TakWolf <takwolf@foxmail.com>
2026-09-17 00:13:44 +0800

    代码整理

--- src/pixel_font_knife/cmap/context.py
+++ src/pixel_font_knife/cmap/context.py
@@ -66,12 +66,15 @@ class CmapContext(UserDict[int, CmapGlyphVariants]):
                     for flavor in glyph_file.flavors:
                         if allowed_flavors is not None and flavor not in allowed_flavors:
                             raise RuntimeError(f"flavor {flavor!r} not allowed:\n'{file_path}'")
+
                         if flavor in glyph_variants:
                             raise RuntimeError(f"flavor {flavor!r} already exists:\n'{file_path}'\n'{glyph_variants[flavor].file_path}'")
+
                         glyph_variants[flavor] = glyph_file
                 else:
                     if None in glyph_variants:
                         raise RuntimeError(f"default flavor already exists:\n'{file_path}'\n'{glyph_variants[None].file_path}'")
+
                     glyph_variants[None] = glyph_file
         return context
 
